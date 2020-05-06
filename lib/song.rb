f9e790303ba1af6c682251ad1f18ffe37c8d3f3d
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-05-06 03:49:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,63 +60,68 @@ class Song
   attr_accessor :name, :artist, :genre
 end
 
-the_day = Song.new("The Day", "John Smith", "classic")
-tomorow = Song.new("Tomorow", "John Smith", "classic")
-a_yearz = Song.new("A Yearz", "Adam Johns", "oldtime")
-a_rabess = Song.new("Arabess", "Habibi Watan", "Rai")
-cus_umak = Song.new("Cus Ummach", "Haifa Wehbe", "Rai")
-
-Song.genre_count
+# the_day = Song.new("The Day", "John Smith", "classic")
+# tomorow = Song.new("Tomorow", "John Smith", "classic")
+# a_yearz = Song.new("A Yearz", "Adam Johns", "oldtime")
+# a_rabess = Song.new("Arabess", "Habibi Watan", "Rai")
+# cus_umak = Song.new("Cus Ummach", "Haifa Wehbe", "Rai")
+#
+# Song.genre_count
 # Song.artist_count
-# class Song
+class Song
 
 
-# attr_accessor = :name, :artist, :genre
-#   @@count = 0
-#   @@artists = []
-#   @@genres = []
-# end
-#
-#
-# def initialize(song_name,artist,genre)
-#   @name = song_name
-#   @artist = artist
-#   @genre = genre
-#   @@count += 1
-#   @@artist << artist
-#   @@genres << genre
-# end
-#
-#   def self.count
-#     @@count
-#   end
-#
-#   def self.artists
-#     @@artists.uniq
-#   end
-#
-#   def self.genres
-#     @@genres.uniq
-#   end
-#
-#   # def self.name
-#   #   name = Hash.new(0)
-#   #   @@name.each {|name| song_name[na] +=1}
-#   #   name
-#   # end
-#
-#   def self.genre_count
-#     genre_count = Hash.new(0)
-#     @@genres.each {|genre| genre_count[genre] += 1}
-#     genre_count
-#   end
-#
-#   def self.artist_count
-#     artist_count = Hash.new(0)
-#     @@artist.each {|artist| artist_count[artist] +=1}
-#     artist_count
-#   end
-#
-# song_name = Song.new(@name, artist, genre)
-# song_name
-# binding.pry
+attr_accessor = :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+end
+
+
+def initialize(song_name,artist,genre)
+  @name = song_name
+  @artist = artist
+  @genre = genre
+  @@count += 1
+  @@artist << artist
+  @@genres << genre
+end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  # def self.name
+  #   name = Hash.new(0)
+  #   @@name.each {|name| song_name[na] +=1}
+  #   name
+  # end
+
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artist.each {|artist| artist_count[artist] +=1}
+    artist_count
+  end
+
+  the_day = Song.new("The Day", "John Smith", "classic")
+  tomorow = Song.new("Tomorow", "John Smith", "classic")
+  a_yearz = Song.new("A Yearz", "Adam Johns", "oldtime")
+  a_rabess = Song.new("Arabess", "Habibi Watan", "Rai")
+  cus_umak = Song.new("Cus Ummach", "Haifa Wehbe", "Rai")
+
+  Song.genre_count
+binding.pry
